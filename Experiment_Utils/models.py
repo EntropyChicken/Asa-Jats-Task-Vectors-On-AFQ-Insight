5b538657a020044f5853a5cedacf9ac249efd698
sam chou <sam@thechous.com>
2025-03-13 00:11:39 -0700

    applying new changes to ALOT of experiments

--- Experiment_Utils/models.py
+++ Experiment_Utils/models.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class Conv1DEncoder_one_tract_fa(nn.Module):
+class Conv1DVariationalEncoder_fa(nn.Module):
     def __init__(self, latent_dims=20, dropout=0.2):
         super().__init__()
         self.conv1 = nn.Conv1d(1, 16, kernel_size=5, stride=2, padding=2)
@@ -35,11 +35,10 @@ class Conv1DEncoder_one_tract_fa(nn.Module):
         x = self.flatten(x) # [64, 64*7]
         mean = self.fc_mean(x)
         logvar = self.fc_logvar(x)    
-        
 
         return mean, logvar
 
-class Conv1DDecoder_one_tract_fa(nn.Module):
+class Conv1DVariationalDecoder_fa(nn.Module):
     def __init__(self, latent_dims=20):
         super().__init__()
         self.fc = nn.Linear(latent_dims, 64 * 7)
@@ -60,11 +59,11 @@ class Conv1DDecoder_one_tract_fa(nn.Module):
         return x
         # return x.view(batch_size, -1)
 
-class Conv1DVariationalAutoencoder_one_tract_fa(nn.Module):
+class Conv1DVariationalAutoencoder_fa(nn.Module):
     def __init__(self, latent_dims=20, dropout=0.0):
         super().__init__()
-        self.encoder = Conv1DEncoder_one_tract_fa(latent_dims, dropout=dropout)
-        self.decoder = Conv1DDecoder_one_tract_fa(latent_dims)
+        self.encoder = Conv1DEncoder_fa(latent_dims, dropout=dropout)
+        self.decoder = Conv1DDecoder_fa(latent_dims)
         self.latent_dims = latent_dims
         
     def reparameterize(self, mean, logvar):
@@ -81,3 +80,63 @@ class Conv1DVariationalAutoencoder_one_tract_fa(nn.Module):
         x_prime = self.decoder(z)
         
         return x_prime, mean, logvar
+    
+class Conv1DEncoder_fa(nn.Module):
+    def __init__(self, latent_dims=20, dropout=0.2):
+        super().__init__()
+        self.conv1 = nn.Conv1d(1, 16, kernel_size=5, stride=2, padding=2)
+        self.conv2 = nn.Conv1d(16, 32, kernel_size=4, stride=2, padding=2)
+        self.conv3 = nn.Conv1d(32, 64, kernel_size=5, stride=2, padding=2)
+        
+        # Instead of directly mapping to latent space, we'll produce two outputs:
+        # mean and log variance (each of size latent_dims)
+        self.conv4 = nn.Conv1d(64, latent_dims, kernel_size=5, stride=2, padding=2)
+        # self.conv4_logvar = nn.Conv1d(64, latent_dims, kernel_size=5, stride=2, padding=2)
+
+        # self.fc_mean = nn.Linear(64*7, latent_dims)
+        # self.fc_logvar = nn.Linear(64*7, latent_dims)
+        
+        self.flatten = nn.Flatten()
+        self.dropout = nn.Dropout(dropout)
+        self.relu = nn.ReLU()
+        
+    def forward(self, x):
+        # x = torch.flatten(x, 1)
+        x = F.relu(self.conv1(x)) # [64, 16, 25]
+        x = self.dropout(x)
+        x = F.relu(self.conv2(x)) # [64, 32, 13]
+        x = self.dropout(x)
+        x = F.relu(self.conv3(x)) # [64, 64, 7]
+        x = self.dropout(x)
+        x = self.conv4(x) # [64, 64, 4]
+
+        return x
+
+class Conv1DDecoder_fa(nn.Module):
+    def __init__(self, latent_dims=20):
+        super().__init__()
+        # self.fc = nn.Linear(latent_dims, 64 * 7)
+        self.deconv1 = nn.ConvTranspose1d(latent_dims, 64, kernel_size=5, stride=2, padding=2, output_padding=0)
+        self.deconv2 = nn.ConvTranspose1d(64, 32, kernel_size=5, stride=2, padding=2, output_padding=0)
+        self.deconv3 = nn.ConvTranspose1d(32, 16, kernel_size=4, stride=2, padding=2, output_padding=1)
+        self.deconv4 = nn.ConvTranspose1d(16, 1, kernel_size=5, stride=2, padding=2, output_padding=1)
+        self.relu = nn.ReLU()
+        
+    def forward(self, x):
+
+        x = F.relu(self.deconv1(x))
+        x = F.relu(self.deconv2(x))
+        x = F.relu(self.deconv3(x))
+        x = self.deconv4(x)
+        return x
+
+class Conv1DAutoencoder_fa(nn.Module):
+    def __init__(self, latent_dims=20, dropout=0.0):
+        super().__init__()
+        self.encoder = Conv1DEncoder_fa(latent_dims, dropout=dropout)
+        self.decoder = Conv1DDecoder_fa(latent_dims)
+        self.latent_dims = latent_dims
+        
+    def forward(self, x):
+        z = self.encoder(x)
+        return self.decoder(z)
